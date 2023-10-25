19f45799007ba53b8d417c771020f643695359cc
Mark A. <mark001616@gmail.com>
2023-10-24 20:54:22 -0400

    Move  logic to index file, add intro message

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import startPage from "./startPage";
 import "./styles.css";
 
-startPage();
+const container = document.querySelector("div#content");
+
+const welcomeMsg = document.createElement("div");
+welcomeMsg.classList.add("header-text");
+welcomeMsg.textContent = "Fancy Restaurant page";
+container.appendChild(welcomeMsg);
+
+const buttonContainer = document.createElement("div");
+buttonContainer.classList.add("buttons-container");
+container.appendChild(buttonContainer);
+const buttonValues = ["Home", "Menu", "Contact"];
+for (const value of buttonValues) {
+  const button = document.createElement("button");
+  button.classList.add("section-button");
+  button.textContent = value;
+  buttonContainer.appendChild(button);
+}
+startPage(container);
--- src/startPage.js
+++ src/startPage.js
@@ -1,12 +1,13 @@
 import restaurantImg from "./Dish on white plate.jpg";
 
-export default function startPage() {
-  const container = document.querySelector("div#content");
+export default function startPage(container) {
+  //   const container = document.querySelector("div#content");
 
-  const welcomeMsg = document.createElement("div");
-  welcomeMsg.classList.add("header-text");
-  welcomeMsg.textContent = "Fancy Restaurant page";
-  container.appendChild(welcomeMsg);
+  const homeMsg = document.createElement("div");
+  homeMsg.classList.add("home-message");
+  homeMsg.textContent =
+    "Welcome to Giorno's where elegance meets exceptional dining. Our carefully curated menu and sophisticated ambiance promise an unforgettable culinary experience. Explore a symphony of flavors in an atmosphere designed to elevate your dining journey. We invite you to join us and savor the perfect blend of taste and elegance.";
+  container.appendChild(homeMsg);
 
   const myImg = new Image();
   myImg.classList.add("image");
