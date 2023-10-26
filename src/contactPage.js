b5cb7653c975779915b335e64555e6ce93d9bfcc
Mark A. <mark001616@gmail.com>
2023-10-26 05:05:34 -0400

    Fix mispell of "address"

--- src/contactPage.js
+++ src/contactPage.js
@@ -4,9 +4,9 @@ export default function contactPage(container) {
   phoneNumber.textContent = "(232)-212-3421";
   contactContainer.appendChild(phoneNumber);
 
-  const adress = document.createElement("div");
-  adress.textContent = "28983 Nolan Shoals, Greenbury, NY, 62637 ";
-  contactContainer.appendChild(adress);
+  const address = document.createElement("div");
+  address.textContent = "28983 Nolan Shoals, Greenbury, NY, 62637 ";
+  contactContainer.appendChild(address);
 
   const email = document.createElement("div");
   email.textContent = "restaurant@email.com";
